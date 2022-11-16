04e8513fbe0368e2a4455b4d202aee522771416e
Ilja Sobolev <ilja.sobolev@ut.ee>
2022-11-16 15:05:14 +0200

    comments

--- tooted.js
+++ tooted.js
@@ -1,15 +1,18 @@
+// kutsutud kui kursor läheb raamatu elemendi peale
 function enter(el) {
     el.children[0].style.filter = "brightness(0.4)";
     el.children[1].style.visibility = "visible";
     el.children[3].style.visibility = "visible";
 }
 
+// kutsutud kui kursor lahkub raamatu elemendist
 function leave(el) {
     el.children[0].style.filter = "brightness(1.0)";
     el.children[1].style.visibility = "hidden";
     el.children[3].style.visibility = "hidden";
 }
 
+// suunab kontakti lehele
 function kontaktid() {
 	window.location.href = "kontakt.html";
 }
