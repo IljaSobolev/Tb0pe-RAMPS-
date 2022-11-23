9e62edb677c07369d23927c0571c442271c24c5b
Ilja Sobolev <ilja.sobolev@ut.ee>
2022-11-23 12:20:14 +0200

    js comment

--- hover.js
+++ hover.js
@@ -1,9 +1,17 @@
+/*
+    kutsutud, kui kursor siseneb raamatu kaanele
+    teeb tausta tumedaks, näitab raamatu kirjeldust ja näitab 'lisa korvi' nuppu
+*/ 
 function enter(el) {
     el.children[0].style.filter = "brightness(0.4)";
     el.children[1].style.visibility = "visible";
     el.children[3].style.visibility = "visible";
 }
 
+/*
+    kutsutud, kuid kursor väljub raamatu kaanelt
+    teeb tausta heledaks, peidab raamatu kirjeldust ja 'lisa korvi' nuppu
+*/
 function leave(el) {
     el.children[0].style.filter = "brightness(1.0)";
     el.children[1].style.visibility = "hidden";
