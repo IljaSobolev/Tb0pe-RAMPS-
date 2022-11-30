bf2c835a4c72a50d296863930a00b026cf834831
Jaanis <jaanissoosaar8@gmail.com>
2022-11-30 12:24:11 +0200

    jaanise kommentaarid

--- menüü.js
+++ menüü.js
@@ -1,4 +1,7 @@
-
+/*  
+    Autor: Jaanis Soosaar
+    Allikas: https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp */
+ 
  /* Menüü avamise funktsioon */   
     function openNav() {
         document.getElementById("mySidebar").style.width = "250px";
